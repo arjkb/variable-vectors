865ac5e1944a555ed0f3b8bf7610aba765610850
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-12-01 17:51:20 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cstdlib>
+
+using namespace std;
+
+void disp_vect(vector <int> v) {
+    for(auto i = v.begin(); i != v.end(); i++)   {
+        cout << *i <<  " ";
+    }
+}
+
+void disp_map(map <int, vector<int>> m)  {
+    for(auto& x: m) {
+        // cout << x.first << " " << x.second << endl;
+        cout << x.first << " -- " ; disp_vect(x.second); cout << endl;
+    }
+}
+
+int main()  {
+    printf("hello!\n");
+    int number_of_normals = 3;
+
+    map <int, vector<int>> vertex_normals;
+
+    for(int i = 0; i < 20; i++) {
+        int vertex = rand() % 5;
+
+        number_of_normals = 1 + rand() % 4; // [1, 4] faces can include this vertex
+        vector <int> normals_of_this_vertex;
+
+        for(int i = 0; i < number_of_normals; i++)  {
+            int random_normal = rand() % 100;
+            normals_of_this_vertex.push_back(random_normal);
+        }
+
+        if (vertex_normals.find(vertex) != vertex_normals.end())    {
+            // a vector of normals for current vertex already exists; extend.
+            printf("\n vertex %d exists!", vertex);
+            vertex_normals[vertex].insert(  vertex_normals[vertex].end(), 
+                                            normals_of_this_vertex.begin(), 
+                                            normals_of_this_vertex.end());
+        } else  {
+            vertex_normals[vertex] = normals_of_this_vertex;
+        }
+        // vertex_normals[vertex].insert(normals_of_this_vertex);
+
+        normals_of_this_vertex.clear();
+
+        printf("\n#%d --------------\n", i);
+        disp_map(vertex_normals);
+    }
+
+    return 0;
+}
