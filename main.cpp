05b759c9748ee89348fb4e5fbd71fe19f61ce285
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-12-01 20:15:57 -0700

    move function definitions to bottom

--- main.cpp
+++ main.cpp
@@ -5,18 +5,8 @@
 
 using namespace std;
 
-void disp_vect(vector <int> v) {
-    for(auto i = v.begin(); i != v.end(); i++)   {
-        cout << *i <<  " ";
-    }
-}
-
-void disp_map(map <int, vector<int>> m)  {
-    for(auto& x: m) {
-        // cout << x.first << " " << x.second << endl;
-        cout << x.first << " -- " ; disp_vect(x.second); cout << endl;
-    }
-}
+void disp_vect(vector <int> v);
+void disp_map(map <int, vector<int>> m);
 
 int main()  {
     printf("hello!\n");
@@ -25,7 +15,7 @@ int main()  {
     map <int, vector<int>> vertex_normals;
 
     for(int i = 0; i < 20; i++) {
-        int vertex = rand() % 5;
+        float vertex = (rand() % 5);
 
         number_of_normals = 1 + rand() % 4; // [1, 4] faces can include this vertex
         vector <int> normals_of_this_vertex;
@@ -37,7 +27,7 @@ int main()  {
 
         if (vertex_normals.find(vertex) != vertex_normals.end())    {
             // a vector of normals for current vertex already exists; extend.
-            printf("\n vertex %d exists!", vertex);
+            printf("\n vertex %f exists!", vertex);
             vertex_normals[vertex].insert(  vertex_normals[vertex].end(), 
                                             normals_of_this_vertex.begin(), 
                                             normals_of_this_vertex.end());
@@ -53,4 +43,19 @@ int main()  {
     }
 
     return 0;
+}
+
+// function definitions:
+
+void disp_vect(vector <int> v) {
+    for(auto i = v.begin(); i != v.end(); i++)   {
+        cout << *i <<  " ";
+    }
+}
+
+void disp_map(map <int, vector<int>> m)  {
+    for(auto& x: m) {
+        // cout << x.first << " " << x.second << endl;
+        cout << x.first << " -- " ; disp_vect(x.second); cout << endl;
+    }
 }
