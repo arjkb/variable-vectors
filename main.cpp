0dbb5fadf6c1b2de8f7959870733ac522cf7f204
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-12-01 21:34:12 -0700

    update disp_vect() and disp_mat()

--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@ class Vertex {
             this->y = y;
             this->z = z;
         }
+        ~Vertex()   {
+            this->x = 0;
+            this->y = 0;
+            this->z = 0;
+
+        }
         bool operator< (const Vertex& x) const;
         friend std::ostream& operator<< (std::ostream&, const Vertex&);
 };
@@ -32,7 +38,7 @@ int main()  {
 
     for(int i = 0; i < 20; i++) {
         // float vertex = (rand() % 5);
-        Vertex vertex(rand() % 5, rand() % 5, rand() % 5);
+        Vertex vertex(rand() % 10, rand() % 10, rand() % 10);
 
         number_of_normals = 1 + rand() % 4; // [1, 4] faces can include this vertex
         vector <int> normals_of_this_vertex;
@@ -51,12 +57,11 @@ int main()  {
         } else  {
             vertex_normals[vertex] = normals_of_this_vertex;
         }
-        // vertex_normals[vertex].insert(normals_of_this_vertex);
 
         normals_of_this_vertex.clear();
 
         printf("\n#%d --------------\n", i);
-        // disp_map(vertex_normals);
+        disp_map(vertex_normals);
     }
 
     return 0;
@@ -72,15 +77,15 @@ std::ostream& operator<< (std::ostream& out, const Vertex& v)  {
     return out << " (" << v.x << ", " << v.y << ", " << v.z << ") ";
 }
 
-// void disp_vect(vector <int> v) {
-//     for(auto i = v.begin(); i != v.end(); i++)   {
-//         cout << *i <<  " ";
-//     }
-// }
+void disp_vect(vector <int> v) {
+    for(auto i = v.begin(); i != v.end(); i++)   {
+        cout << *i <<  " ";
+    }
+}
 
-// void disp_map(map <int, vector<int>> m)  {
-//     for(auto& x: m) {
-//         // cout << x.first << " " << x.second << endl;
-//         cout << x.first << " -- " ; disp_vect(x.second); cout << endl;
-//     }
-// }
+void disp_map(map <Vertex, vector<int>> m)  {
+    for(auto& x: m) {
+        // cout << x.first << " " << x.second << endl;
+        cout << x.first << " -- " ; disp_vect(x.second); cout << endl;
+    }
+}
