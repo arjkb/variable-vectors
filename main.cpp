29c1705ac7eaa47fe8a4010c9c491c0c28b60a27
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-12-01 21:27:49 -0700

    define operator< and operator<< for Vertex

--- main.cpp
+++ main.cpp
@@ -5,17 +5,34 @@
 
 using namespace std;
 
+class Vertex {
+    private:    
+        int x;
+        int y;
+        int z;
+
+    public:
+        Vertex(int x, int y, int z) {
+            this->x = x;
+            this->y = y;
+            this->z = z;
+        }
+        bool operator< (const Vertex& x) const;
+        friend std::ostream& operator<< (std::ostream&, const Vertex&);
+};
+
 void disp_vect(vector <int> v);
-void disp_map(map <int, vector<int>> m);
+void disp_map(map <Vertex, vector<int>> m);
 
 int main()  {
     printf("hello!\n");
     int number_of_normals = 3;
 
-    map <int, vector<int>> vertex_normals;
+    map <Vertex, vector<int>> vertex_normals;
 
     for(int i = 0; i < 20; i++) {
-        float vertex = (rand() % 5);
+        // float vertex = (rand() % 5);
+        Vertex vertex(rand() % 5, rand() % 5, rand() % 5);
 
         number_of_normals = 1 + rand() % 4; // [1, 4] faces can include this vertex
         vector <int> normals_of_this_vertex;
@@ -27,7 +44,7 @@ int main()  {
 
         if (vertex_normals.find(vertex) != vertex_normals.end())    {
             // a vector of normals for current vertex already exists; extend.
-            printf("\n vertex %f exists!", vertex);
+            // printf("\n vertex %lf exists!", vertex);
             vertex_normals[vertex].insert(  vertex_normals[vertex].end(), 
                                             normals_of_this_vertex.begin(), 
                                             normals_of_this_vertex.end());
@@ -39,7 +56,7 @@ int main()  {
         normals_of_this_vertex.clear();
 
         printf("\n#%d --------------\n", i);
-        disp_map(vertex_normals);
+        // disp_map(vertex_normals);
     }
 
     return 0;
@@ -47,15 +64,23 @@ int main()  {
 
 // function definitions:
 
-void disp_vect(vector <int> v) {
-    for(auto i = v.begin(); i != v.end(); i++)   {
-        cout << *i <<  " ";
-    }
+bool Vertex::operator< (const Vertex& v) const  {
+    return (x < v.x) && (y < v.y) && (z < v.z);
 }
 
-void disp_map(map <int, vector<int>> m)  {
-    for(auto& x: m) {
-        // cout << x.first << " " << x.second << endl;
-        cout << x.first << " -- " ; disp_vect(x.second); cout << endl;
-    }
+std::ostream& operator<< (std::ostream& out, const Vertex& v)  {
+    return out << " (" << v.x << ", " << v.y << ", " << v.z << ") ";
 }
+
+// void disp_vect(vector <int> v) {
+//     for(auto i = v.begin(); i != v.end(); i++)   {
+//         cout << *i <<  " ";
+//     }
+// }
+
+// void disp_map(map <int, vector<int>> m)  {
+//     for(auto& x: m) {
+//         // cout << x.first << " " << x.second << endl;
+//         cout << x.first << " -- " ; disp_vect(x.second); cout << endl;
+//     }
+// }
